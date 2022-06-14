778e2dda9fb6b089bb7cf67393b0cfca17280f23
bharatadk <bharatadk.on@gmail.com>
2022-06-14 22:54:18 +0545

    random color changer added finally with clipboard copy functionality

--- random_color_changer/script/script.js
+++ random_color_changer/script/script.js
@@ -6,6 +6,7 @@ const color_generator=()=>{
 	const color = rgb.toString()
 	document.body.style.backgroundColor = "rgb"+"("+color+")";
 	document.getElementById('color-code').innerText="rgb"+" ("+rgb+")";
+	navigator.clipboard.writeText("rgb"+" ("+rgb+")")
 }
 document.getElementById('btn').addEventListener("click",color_generator)
 
